0d5876fbb965a11f95f47692125f124f6a17d708
Ronak Radadiya <ronakradadiya7@gmail.com>
2025-06-08 23:53:17 +0530

    added debouncing and caching to autocomplete problem

--- autocomplete/README.md
+++ autocomplete/README.md
@@ -1,12 +1,11 @@
-# React + Vite
+# Autocomplete Component
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Functionalities implemented
 
-Currently, two official plugins are available:
+1. Debouncing
+2. onBlur close dropdown, onFocus open dropdown
+3. Caching
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+## TODO
+1. on pressing down arrow key, it should highlight the appropriate result.
+2. Add accessibility
--- autocomplete/src/App.jsx
+++ autocomplete/src/App.jsx
@@ -4,21 +4,37 @@ import "./App.css";
 function App() {
   const [results, setResults] = useState([]);
   const [input, setInput] = useState("");
+  const [showResults, setShowResults] = useState(false);
+  const [cache, setCache] = useState({});
 
   const fetchData = async () => {
+    if (cache[input]) {
+      console.log('Cache returned for', input)
+      setResults(cache[input])
+      return;
+    }
+
+    console.log("API Call", input);
+
     try {
       const data = await fetch(
         `https://dummyjson.com/recipes/search?q=${input}`
       );
       const json = await data.json();
-      setResults(json?.recipes ?? []);
+      const apiResults = json?.recipes ?? [];
+      setResults(apiResults);
+      setCache((prev) => ({
+        ...prev,
+        [input]: apiResults,
+      }));
     } catch (error) {
       console.error(error);
     }
   };
 
   useEffect(() => {
-    fetchData();
+    const timer = setTimeout(fetchData, 300);
+    return () => clearTimeout(timer);
   }, [input]);
 
   return (
@@ -30,15 +46,27 @@ function App() {
           className="search-input"
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          onFocus={() => setShowResults(true)}
+          onBlur={() => setShowResults(false)}
         />
       </div>
-      <div className="results-container">
-        {results.map((resultItem) => (
-          <span key={resultItem.id} className="result">
-            {resultItem.name}
-          </span>
-        ))}
-      </div>
+      {showResults && (
+        <div className="results-container">
+          {results.length ? (
+            <>
+              {results.map((resultItem) => (
+                <span key={resultItem.id} className="result">
+                  {resultItem.name}
+                </span>
+              ))}
+            </>
+          ) : (
+            <>
+              <span className="no-results">No results found</span>
+            </>
+          )}
+        </div>
+      )}
     </>
   );
 }
